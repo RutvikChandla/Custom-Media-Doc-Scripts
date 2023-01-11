f89815dbf8012d8f2248470e968a8b0c4b5dcd8e
Rutvik Chandla <97675949+RutvikChandla@users.noreply.github.com>
2023-01-11 12:39:16 +0530

    Create mac.cs

---
+++ JSONWIRE/mac.cs
@@ -0,0 +1,41 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+class FileUpload
+{
+    static void Main(string[] args)
+    {
+        var caps = new DesiredCapabilities();
+        caps.SetCapability("os", "OS X");
+        caps.SetCapability("os_version", "Big Sur");
+        caps.SetCapability("browser", "chrome");
+        caps.SetCapability("browser_version", "latest");
+        caps.SetCapability("browserstack.local", "false");
+        caps.SetCapability("browserstack.uploadMedia", "media://<FILE_HASHED_ID>");
+        IWebDriver driver = new RemoteWebDriver(new Uri("https://YOUR_USERNAME:YOUR_ACCESSKEY@hub-cloud.browserstack.com/wd/hub"), caps);
+        try
+        {
+            driver.Navigate().GoToUrl("http://www.fileconvoy.com");
+            driver.FindElement(By.Id("upfile_0")).SendKeys("/Users/test1/Documents/<MEDIA_FOLDER><IMAGE_NAME>.<IMAGE_EXT>");
+            driver.ExecuteScript("document.getElementById('readTermsOfUse').click();");
+            driver.FindElement(By.Name("upload_button")).Submit();
+            string ele = driver.FindElement(By.Id("TopMessage")).Text;
+            if (ele.Contains("successfully uploaded"))
+            {
+                driver.ExecuteScript("browserstack_executor: {'action': 'setSessionStatus', 'arguments': {'status':'passed','reason': 'File upload successful'}}");
+            }
+            else
+            {
+                driver.ExecuteScript("browserstack_executor: {'action': 'setSessionStatus', 'arguments': {'status':'failed','reason': 'File upload failed'}}");
+            }
+        }
+        catch (Exception e)
+        {
+            driver.ExecuteScript("browserstack_executor: {'action': 'setSessionStatus', 'arguments': {'status':'failed','reason': 'Something wrong with script'}}");
+        }
+        finally
+        {
+            driver.Quit();
+        }
+    }
+}
